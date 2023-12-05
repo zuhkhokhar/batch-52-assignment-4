8a077966cbbbc9005a0d1cbdef1fd74646d2f86b
ZIA UL HAQ <32288964+zuhkhokhar@users.noreply.github.com>
2023-12-05 21:09:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+const quote = "Life is like riding a bicycle. To keep your balance, you must keep moving.";
+const authorName = "Albert Einstein";
+console.log(`${authorName} once said: "${quote}"`);
---
+++ app.ts
@@ -0,0 +1,5 @@
+const quote =
+  "Life is like riding a bicycle. To keep your balance, you must keep moving.";
+const authorName = "Albert Einstein";
+
+console.log(`${authorName} once said: "${quote}"`);
